efb09d9a60d72bd050f01e1154d9e451be20fae0
Daniel Pañeda <dpaneda@gmail.com>
2011-08-16 00:21:03 +0200

    Adding more config files and install command

---
+++ home/.vimrc
@@ -0,0 +1,5 @@
+syntax on
+set ts=4
+set expandtab
+set shiftwidth=4
+set fileencodings=utf-8,latin2
---
+++ home_install.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+mkdir -p ~/.old_config/.config
+
+function link_with_backup {
+    config=$1
+    backup_dir=$2
+
+    if [ -L ~/$config ]
+    then
+        echo "Already linked: $config"
+        return
+    elif [ ! -e $backup_dir/$config ] && [ -e ~/$config ]
+    then
+        echo "Backuping $config on $backup_dir" 
+        mv ~/$config $backup_dir
+    fi
+    echo "Linking $config"
+    ln -s $PWD/$config ~/$config
+}
+
+cd home
+for config in .*
+do
+    # We .config dir because we don't want to link them itself
+    [[ $config == ".config" ]] && continue
+    link_with_backup $config ~/.old_config
+done
+
+for config in .config/*
+do
+    link_with_backup $config ~/.old_config/.config
+done
+cd -
