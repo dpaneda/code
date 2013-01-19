d59b8a4d8cd4022e807695d6dcf310c13830c488
Daniel Pañeda <dpaneda@gmail.com>
2013-01-19 16:42:58 +0100

    Cache to rsee

--- home/shared/bin/rsee.sh
+++ home/shared/bin/rsee.sh
@@ -1,12 +1,20 @@
 #!/bin/zsh
 # Script to see a series of pictures over http, as feh does not support ranges
 
+cd /tmp
 for arg in $*
 do
-    dir=`mktemp -d`
+    dir=`md5sum <<<"$arg"`
 
-    cd $dir
-    curl -O $arg
+    if [ -d $dir ]
+    then
+        cd $dir
+    else
+        mkdir -p $dir
+        cd $dir
+        curl -O $arg
+    fi
+
     feh -F *
-    cd -
 done
+cd -
