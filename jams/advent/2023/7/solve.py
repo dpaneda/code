aae33cf6820e78f84f24db4a22a6d87f018f9cd6
Daniel Pañeda <dpaneda@gmail.com>
2023-12-09 10:58:42 +0100

    Add problem 5 - advent 2023

---
+++ jams/advent/2023/7/solve.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+
+import sys
+from collections import Counter
+from dataclasses import dataclass
+from functools import total_ordering
+
+cards = "023456789TJQKA"
+card_rank = {c: cards.index(c) for c in cards}
+
+
+@dataclass
+@total_ordering
+class Hand():
+    raw: str
+    hand: Counter
+    bid: int
+
+    def __init__(self, s):
+        s, bid = s.split()
+        self.raw = s
+        self.hand = Counter(s)
+        self.bid = int(bid)
+
+    def __eq__(self, other):
+        return self.hand == other.hand
+
+    def order_counters(self):
+        return sorted(self.hand.most_common(),
+                      key=lambda x: 1000 * x[1] + card_rank[x[0]],
+                      reverse=True)
+
+    def hand_type(self):
+        l = [x[1] for x in self.hand.most_common()]
+        if l == [5]:
+            return 6
+        elif l == [4, 1]:
+            return 5
+        elif l == [3, 2]:
+            return 4
+        elif l == [3, 1, 1]:
+            return 3
+        elif l == [2, 2, 1]:
+            return 2
+        elif l == [2, 1, 1, 1]:
+            return 1
+        return 0
+
+    def __lt__(self, other):
+        if self.hand_type() != other.hand_type():
+            return self.hand_type() < other.hand_type()
+
+        for a, b in zip(self.raw, other.raw):
+            if card_rank[a] != card_rank[b]:
+                return card_rank[a] < card_rank[b]
+        return False
+
+        a = self.order_counters()
+        b = other.order_counters()
+
+        while a:
+            card, _ = a.pop(0)
+            card2, _ = b.pop(0)
+            rank1, rank2 = card_rank[card], card_rank[card2]
+            if rank1 != rank2:
+                return rank1 < rank2
+        return False
+
+
+hands = sorted(map(Hand, sys.stdin))
+values = [hand.bid * (i + 1) for i, hand in enumerate(hands)]
+print(sum(values))
